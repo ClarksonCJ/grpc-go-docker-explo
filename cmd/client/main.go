e41da7bea8ecf42259614657f84f17622aeb723d
Chris Clarkson <chris.clarkson@cjcsoftwaresolutions.co.uk>
2017-06-01 21:13:44 +0100

    Client Server Implemented

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,13 +5,34 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
+
+	pb "github.com/ClarksonCJ/grpc-in-docker/pkg/protobuf"
+	"google.golang.org/grpc"
 )
 
-var address = flag.String("Address", "localhost", "Server address of the gRPC remote application host")
-var port = flag.Int("Port", 30555, "Port number exposed by remote application host")
+var address = flag.String("Address", "localhost:30551", "Server address of the gRPC remote application host")
+var name = flag.String("Name", "world", "Default string to send to gRPC server")
 
 func main() {
 	flag.Parse()
 
+	// Setup connection to the server
+	conn, err := grpc.Dial(*address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response
+	name := *name
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
 }
